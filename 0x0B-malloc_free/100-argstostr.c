95de86225a76c015576c57b6f97193f413f2d73c
jobukk <jobkipkurui3843@gmail.com>
2023-11-21 15:20:08 -0800

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,43 +1,44 @@
-#include "main.h"
-#include <stdlib.h>
+#include"main.h"
+#include<stdlib.h>
+#include<string.h>
 
 char *argstostr(int ac, char **av)
 {
-    int size, i, n;
-    size_t total_length =0;
-   
-    if (ac <= 0 || av == NULL)
-        return (NULL);
-    
-   
-    
-    for (i = 0; i < ac; i++)
-    {
-        if (av[i] == NULL)
-        {
-            return (NULL);
-        }
-        total_length += strlen(av[i]) + i;
-    }
-    
-    char *result = (char *)malloc(total_length + 1);
-    if (result == NULL)
-        return (NULL);
-        
-    size_t current_position = 0;
-    
-    for (n = 0; n < ac; n++)
-    {
-        strcpy(result + current_position, av[n]);
-        
-        current_position += strlen(av[n]);
-        
-        result[current_position] = '\n';
-        
-        current_position++;
-    }
-    
-    result[current_position] = '\0';
-    return result;
-    
+int size, i, n;
+size_t total_length = 0;
+
+if (ac <= 0 || av == NULL)
+return (NULL);
+
+
+
+for (i = 0; i < ac; i++)
+{
+if (av[i] == NULL)
+{
+return (NULL);
+}
+total_length += strlen(av[i]) + i;
+}
+
+char *result = (char *)malloc(total_length + 1);
+if (result == NULL)
+return (NULL);
+
+size_t current_position = 0;
+
+for (n = 0; n < ac; n++)
+{
+strcpy(result + current_position, av[n]);
+
+current_position += strlen(av[n]);
+
+result[current_position] = '\n';
+
+current_position++;
+}
+
+result[current_position] = '\0';
+return (result);
+
 }
