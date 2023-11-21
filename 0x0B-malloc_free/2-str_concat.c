363e3ebcb0be1a34ad789eca8701b46ecad7f409
jobukk <jobkipkurui3843@gmail.com>
2023-11-20 21:06:05 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,8 @@
 char *str_concat(char *s1, char *s2)
 {
 int size1, size2, total;
+char *result;
+
 if (s1 == NULL)
 {
 s1 = "";
@@ -24,7 +26,6 @@ size1 = strlen(s1);
 size2 = strlen(s2);
 total = size1 + size2;
 
-char *result;
 result = malloc(total);
 if (result == NULL)
 {
