60cfc4399f9c764a690948a54b31b86385fd1620
jobukk <jobkipkurui3843@gmail.com>
2023-11-26 00:45:38 -0800

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
-/**
-* _calloc - allocate memory using malloc and initialize it to zero
-*@nmemb: number of elements
-*@size: size of the memory block to be allocated
-*
-*Return: poiner to the address of the memory block
-*/
+
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *block;
-	unsigned int i;
+if (nmemb == 0 || size == 0)
+	return (NULL);
+size_t total_size = nmemb * size;
+void *ptr = malloc(total_size);
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	block = malloc(nmemb * size);
-	if (block != NULL)
-	{
-		for (i = 0; i < (nmemb * size); i++)
-			block[i] = 0;
-		return (block);
-	}
-	else
-		return (NULL);
+if (ptr == NULL)
+	return (NULL);
+
+memset(ptr, 0, total_size);
+
+return (ptr);
 }
