0a199ff5d311bf6e423c04a0ca732d2fac99ea91
jobukk <jobkipkurui3843@gmail.com>
2023-11-27 11:01:08 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - all compsite of dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the dog owner
+ *
+ * Description: compsite of the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
